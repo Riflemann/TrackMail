ceae5f2b6dd9fd393bec695c42fddc011f0490c2
Konstantin <kdanilov1911@gmail.com>
2023-08-15 22:36:32 +0300

    first commit

---
+++ src/main/java/com/trackmail/TrackMailApplication.java
@@ -0,0 +1,13 @@
+package com.trackmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trackmail/TrackMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
